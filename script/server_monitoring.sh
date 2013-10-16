f9d41697e0a0ac5f5e82984b05195ff37b953393
root <root@ichi.(none)>
2013-10-16 23:59:17 +0000

    Fix variable.

--- script/server_monitoring.sh
+++ script/server_monitoring.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-$LICENSE_KEY=`cat /home/rails/config/newrelic.yml | grep license_key | awk '{print $2}' | tr -d "'"`
+LICENSE_KEY=`cat /home/rails/config/newrelic.yml | grep license_key | awk '{print $2}' | tr -d "'"`
 echo deb http://apt.newrelic.com/debian/ newrelic non-free >> /etc/apt/sources.list.d/newrelic.list
 wget -O- https://download.newrelic.com/548C16BF.gpg | apt-key add -
 apt-get update

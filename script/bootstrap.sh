57c77e8fcfd306a5c21643ac78d252c9f2d5fbc0
root <root@ichi.(none)>
2013-10-16 23:04:22 +0000

    Remove -y from github, fix app path.

--- script/bootstrap.sh
+++ script/bootstrap.sh
@@ -1,13 +1,14 @@
+#!/usr/bin/env bash
 : ${APP_NAME:?"You need to set the APP_NAME environment variable."}
 cd /home
 rm -rf rails
 apt-get -y update && apt-get -y install git siege
 echo -e "Host github.com\n\tStrictHostKeyChecking no\n" >> ~/.ssh/config
-git clone -y git@github.com:1337807/rainbow.git rails
+git clone git@github.com:1337807/rainbow.git rails
 chown rails:rails rails
 chown -R rails:www-data rails/*
-cd rails/
-bundle
+cd /home/rails
+bundle install
 RAILS_ENV=production rake db:setup
 perl -pi -e "s/Ichi/$APP_NAME/g" config/newrelic.yml
 pkill -f unicorn

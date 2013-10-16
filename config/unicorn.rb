f42b5072ada7a308cb6420a95d4850c9d7e1023a
root <root@ichi.(none)>
2013-10-16 21:43:14 +0000

    Fixed unicorn.rb and newrelic.yml.

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,1 +1,8 @@
+listen "127.0.0.1:8080"
+worker_processes 2
+user "rails"
+working_directory "/home/rails"
+pid "/home/unicorn/pids/unicorn.pid"
 preload_app true
+stderr_path "/home/unicorn/log/unicorn.log"
+stdout_path "/home/unicorn/log/unicorn.log"
